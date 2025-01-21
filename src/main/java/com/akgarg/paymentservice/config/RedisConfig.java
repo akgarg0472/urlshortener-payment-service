e24669930524f76e96f27d673f8fa078a7cc923e
akgarg0472 <akgarg0472@gmail.com>
2025-01-21 17:54:06 +0530

    feat: info and health actuator endpoints

--- src/main/java/com/akgarg/paymentservice/config/RedisConfig.java
+++ src/main/java/com/akgarg/paymentservice/config/RedisConfig.java
@@ -12,7 +12,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@Profile({"prod", "PROD"})
+@Profile("prod")
 public class RedisConfig {
 
     @Bean
